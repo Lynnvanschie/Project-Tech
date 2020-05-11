148c36285c97575d346ec5b565030196e99213c7
Lynnvanschie <lynn1108@live.nl>
2020-05-11 14:22:45 +0200

    Added css and images to static files, added a new about route

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const express = require('express')
 express()
   .use(express.static('static')) //Serveert static files
   .get('/', onhome)
+  .get('/about', about)
   .listen(1900)
 
 function onhome(req,res) {
-  res.send('<h1>Hello Client</h1>\n')
+  res.status(200).send('<h1>Hello Client</h1>\n')
+}
+
+function about(req,res) {
+  res.status(200).send('<h1>This is a server</h1>\n')
 }
