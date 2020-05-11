5646ffec36a7b028ee4a6309ea1fcff0e9844b66
Lynnvanschie <lynn1108@live.nl>
2020-05-11 13:39:20 +0200

    Updated express server

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express')
-const app = express();
 
 express()
+  .use(express.static('static')) //Serveert static files
   .get('/', onhome)
   .listen(1900)
 
 function onhome(req,res) {
-  res.send('<h1>Hello Client</h1>\n');
+  res.send('<h1>Hello Client</h1>\n')
 }
-//This code is supposed to serve static pages but it doesn't work
-app.use('/static', express.static('static'));
