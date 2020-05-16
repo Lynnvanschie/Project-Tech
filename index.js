aae84548445be7fb5b7084b927d8282191bbf4e5
Lynnvanschie <lynn1108@live.nl>
2020-05-16 15:12:05 +0200

    Added routes to serve audio, video and images

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const express = require('express')
+//camelcase
+const camelCase = require('camelcase');
+console.log(camelCase('test-this-package'));
+
+//Express aanroepen
+const express = require('express');
+const app = express();
 
 express()
   .use(express.static('static')) //Serveert static files
   .get('/', onhome)
   .get('/about', about)
+  .get('/audio', audio)
+  .get('/video', video)
+  .get('/image', image)
   .listen(1900)
 
 function onhome(req,res) {
@@ -12,4 +21,18 @@ function onhome(req,res) {
 
 function about(req,res) {
   res.status(200).send('<h1>This is a server</h1>\n')
+}
+
+function audio(req,res) {
+  res.sendFile('./static/audio/Adoreyou.mp3', { root: __dirname });
+  //.sendFile serveert een bestand.
+  //De root moet aangegeven worden. __dirname is een globale node.js variabele die het 'currently running file' aangeeft.
+}
+
+function video(req,res) {
+  res.sendFile('/static/video/Falling.mp4', { root: __dirname })
+}
+
+function image(req,res) {
+  res.sendFile('/static/img/landschap.jpg', { root: __dirname })
 }
