a6939d1888d25de46de72ce234ec0b91669901ff
Lynnvanschie <lynn1108@live.nl>
2020-06-04 14:30:01 +0200

    converted static pages for project tech assesment 1

---
+++ static pages/js/script.js
@@ -0,0 +1,14 @@
+// console.log('test');
+//
+// var filterknop = document.querySelector('.f-knop');
+// var filterblok = document.querySelector('.filters');
+//
+// function showKnop(){
+// 	filterblok.classList.toggle('showfilters');
+// 	console.log('testfunction');
+// }
+//
+// filterknop.addEventListener('click', showKnop);
+
+// console.log(filterblok)
+// console.log(filterknop)
